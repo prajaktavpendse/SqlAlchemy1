5344cc6e21f6faabd82ebb210831ea22c563f28b
Prajakta <prajaktavpendse@gmail.com>
2019-12-07 18:48:18 -0500

    Feature: Added sqlAlchemy file with all initial required code

---
+++ sqlAlchemy.py
@@ -0,0 +1,102 @@
+import sqlalchemy
+from sqlalchemy import create_engine, CheckConstraint, Numeric
+from sqlalchemy import create_engine, MetaData, Table, Integer, String, Column, Text, DateTime, Boolean, ForeignKey, SmallInteger
+from datetime import datetime
+from sqlalchemy import insert
+from sqlalchemy import select
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+from sqlalchemy import update
+from sqlalchemy import delete
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.ext.declarative import declarative_base
+
+
+engine = create_engine('sqlite:///sqlalchemy_tuts.db')
+engine.connect()
+
+print(engine)
+
+metadata = MetaData()
+
+customers = Table('customers', metadata,
+                  Column('id', Integer(), primary_key=True),
+                  Column('first_name', String(100), nullable=False),
+                  Column('last_name', String(100), nullable=False),
+                  Column('username', String(50), nullable=False),
+                  Column('email', String(200), nullable=False),
+                  Column('address', String(200), nullable=False),
+                  Column('town', String(50), nullable=False),
+                  Column('created_on', DateTime(), default=datetime.now),
+                  Column('updated_on', DateTime(), default=datetime.now, onupdate=datetime.now)
+                  )
+
+items = Table('items', metadata,
+              Column('id', Integer(), primary_key=True),
+              Column('name', String(200), nullable=False),
+              Column('cost_price', Numeric(10, 2), nullable=False),
+              Column('selling_price', Numeric(10, 2), nullable=False),
+              Column('quantity', Integer(), nullable=False),
+              CheckConstraint('quantity > 0', name='quantity_check')
+              )
+
+orders = Table('orders', metadata,
+               Column('id', Integer(), primary_key=True),
+               Column('customer_id', ForeignKey('customers.id')),
+               Column('date_placed', DateTime(), default=datetime.now),
+               Column('date_shipped', DateTime())
+               )
+
+order_lines = Table('order_lines', metadata,
+                    Column('id', Integer(), primary_key=True),
+                    Column('order_id', ForeignKey('orders.id')),
+                    Column('item_id', ForeignKey('items.id')),
+                    Column('quantity', Integer())
+                    )
+
+conn = engine.connect()
+
+Base = declarative_base()
+
+
+class Customer(Base):
+    __tablename__ = 'customers'
+    id = Column(Integer(), primary_key=True)
+    first_name = Column(String(100), nullable=False)
+    last_name = Column(String(100), nullable=False)
+    username = Column(String(50), nullable=False)
+    email = Column(String(200), nullable=False)
+    created_on = Column(DateTime(), default=datetime.now)
+    updated_on = Column(DateTime(), default=datetime.now, onupdate=datetime.now)
+    orders = relationship("Order", backref='customer')
+
+
+class Item(Base):
+    __tablename__ = 'items'
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(200), nullable=False)
+    cost_price = Column(Numeric(10, 2), nullable=False)
+    selling_price = Column(Numeric(10, 2), nullable=False)
+
+
+#     orders = relationship("Order", backref='customer')
+
+
+class Order(Base):
+    __tablename__ = 'orders'
+    id = Column(Integer(), primary_key=True)
+    customer_id = Column(Integer(), ForeignKey('customers.id'))
+    date_placed = Column(DateTime(), default=datetime.now)
+    line_items = relationship("OrderLine", secondary="order_lines", backref='order')
+
+
+class OrderLine(Base):
+    __tablename__ = 'order_lines'
+    id = Column(Integer(), primary_key=True)
+    order_id = Column(Integer(), ForeignKey('orders.id'))
+    item_id = Column(Integer(), ForeignKey('items.id'))
+    quantity = Column(SmallInteger())
+    item = relationship("Item")
+
+
+Base.metadata.create_all(engine)
