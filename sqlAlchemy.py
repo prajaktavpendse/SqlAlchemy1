e83ce001cbb382a9d1214bd99465752aff0cf268
Prajakta <prajaktavpendse@gmail.com>
2019-12-07 20:09:22 -0500

    Task: Offset

--- sqlAlchemy.py
+++ sqlAlchemy.py
@@ -250,3 +250,6 @@ print(session.query(Item).filter(not_(Item.name.like("W%"))).all())
 print(session.query(Customer).limit(2).all())
 print(session.query(Customer).filter(Customer.address.ilike("%avenue")).limit(2).all())
 
+print(session.query(Customer).limit(2).offset(2).all())
+
+
